2ad27b9d8bd63a3bb58396e987a2c87d6f00287a
Chelsea Holland Komlo <me@chelseakomlo.com>
2017-10-05 21:33:29 -0400

    simple implementation for finding similar-ish router names

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,130 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn read_consensus() -> String {
+    let path = Path::new("consensus");
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Ok(file) => file,
+        Err(why) => panic!("couldn't open {}: {}", display,
+                           why.description()),
+    };
+
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Ok(_) => println!("successfully read file!"),
+        Err(why) => panic!("couldn't read {}: {}", display,
+                           why.description()),
+    }
+    s
+}
+
+fn is_router_line(line: &str) -> bool {
+    let mut parts = line.split(" ");
+    match parts.nth(0) {
+        Some(n) => n == "r",
+        None => false,
+    }
+}
+
+fn is_hamming_match(first_str: &str, second_str: &str) -> bool {
+    let mut first = first_str.chars();
+    let mut second = second_str.chars();
+
+    let mut hamming_count = 0;
+
+    while hamming_count <= 4 {
+        let first_char = match first.next() {
+            Some(n) => n,
+            None => break,
+        };
+        let second_char = match second.next() {
+            Some(n) => n,
+            None => break,
+        };
+
+        if first_char != second_char {
+            hamming_count += 1;
+        }
+    }
+
+    hamming_count < 3
+}
+
+fn get_router_name(line: &str) -> Result<&str, &str> {
+    let name = line.split(" ").nth(1);
+    match name {
+        Some(n) => return Ok(n),
+        None => return Err("malformed router line"),
+    }
+}
+
+struct HammingMatch<'a>{
+    count: u32,
+    matches: Vec<&'a str>,
+}
+
+fn contains_match(name: &str, matches: &Vec<&str>) -> bool {
+    let hamming_matches = matches.iter()
+        .find(|x| is_hamming_match(&name, x));
+
+    hamming_matches.is_some()
+}
+
+fn get_hamming_matches(router_lines: Vec<&str>) -> Vec<HammingMatch> {
+    let mut all_router_matches:Vec<HammingMatch> = Vec::new();
+
+    for line in router_lines {
+        let name = match get_router_name(line) {
+            Ok(n) => n,
+            Err(_) => continue,
+        };
+
+        let num_matched;
+
+        {
+            num_matched = all_router_matches.iter_mut()
+                .filter_map(|x|  {
+                    if contains_match(&name, &x.matches) {
+                        x.matches.push(name);
+                        x.count += 1;
+                        return Some(x)
+                    }
+                    None
+                }).count();
+        }
+
+        if num_matched == 0 {
+            let new_match = HammingMatch {
+                count: 0,
+                matches: vec![name],
+            };
+
+            all_router_matches.push(new_match);
+        }
+
+    }
+
+    all_router_matches
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Getting similar router lines....");
+
+    let consensus = read_consensus();
+    let router_lines = consensus.split("\n")
+                                .filter(|x| is_router_line(x))
+                                .collect::<Vec<&str>>();
+
+    // Only interested in groups of size greater than one
+    let hamming_matches = get_hamming_matches(router_lines).iter()
+                                .filter(|x| x.count > 1)
+                                .map(|x| x.matches.join(" ,"))
+                                .collect::<Vec<String>>()
+                                .join(" \n\n");
+
+    print!("{}", hamming_matches);
 }
